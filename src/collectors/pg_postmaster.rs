059d0d4f09de6f755ea84c91d914b787b4888efd
RustWizard <rustwizard.0@gmail.com>
2025-02-24 22:30:09 +0300

    move collectors to module (#14)

---
+++ src/collectors/pg_postmaster.rs
@@ -0,0 +1,93 @@
+use std::sync::{Arc, Mutex};
+
+use prometheus::core::{Desc, Opts, Collector};
+use prometheus::Gauge;
+use prometheus::proto;
+use sqlx::PgPool;
+
+
+
+const POSTMASTER_QUERY: &str = "SELECT extract(epoch from pg_postmaster_start_time)::FLOAT8 as start_time_seconds from pg_postmaster_start_time()";
+
+#[derive(sqlx::FromRow, Debug)]
+pub struct PGPostmasterStats {
+    start_time_seconds: f64
+}
+
+impl PGPostmasterStats {
+    pub fn new() -> PGPostmasterStats {
+        PGPostmasterStats{
+            start_time_seconds: (0.0),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct PGPostmasterCollector {
+    db: PgPool,
+    data: Arc<Mutex<PGPostmasterStats>>,
+    descs: Vec<Desc>,
+    start_time_seconds: Gauge,
+}
+
+
+pub fn new<S: Into<String>>(namespace: S, db: PgPool) -> PGPostmasterCollector {
+    PGPostmasterCollector::new(namespace, db)
+}
+
+
+impl PGPostmasterCollector {
+    pub fn new<S: Into<String>>(namespace: S, db: PgPool) -> PGPostmasterCollector {
+        let namespace = namespace.into();
+
+        let start_time_seconds = Gauge::with_opts(
+            Opts::new(
+                "start_time_seconds",
+                "Time at which postmaster started",
+            )
+            .namespace(namespace.clone()),
+        )
+        .unwrap();
+
+        let mut descs = Vec::new();
+        descs.extend(start_time_seconds.desc().into_iter().cloned());
+
+        PGPostmasterCollector{
+            db: db,
+            data: Arc::new(Mutex::new(PGPostmasterStats::new())),
+            descs: descs,
+            start_time_seconds: start_time_seconds,
+        }
+    }
+
+    pub async fn update(&self) -> Result<(), anyhow::Error> {
+        let myabe_stats = sqlx::query_as::<_, PGPostmasterStats>(POSTMASTER_QUERY).fetch_optional(&self.db).await?;
+       
+        if let Some(stats) = myabe_stats {
+            let mut data_lock = self.data.lock().unwrap();
+            data_lock.start_time_seconds         = stats.start_time_seconds;
+        }
+    
+        Ok(())
+    }
+}
+
+
+
+impl Collector for PGPostmasterCollector {
+    fn desc(&self) -> Vec<&Desc> {
+        self.descs.iter().collect()
+    }
+
+    fn collect(&self) -> Vec<proto::MetricFamily> {
+        // collect MetricFamilys.
+        let mut mfs = Vec::with_capacity(1);
+        
+        let data_lock = self.data.lock().unwrap();
+        self.start_time_seconds.set(data_lock.start_time_seconds);
+
+        mfs.extend(self.start_time_seconds.collect());
+        mfs
+
+    }
+}
