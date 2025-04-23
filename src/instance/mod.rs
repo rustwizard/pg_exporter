2ea5e7b519b9d61de7d97746d1cd77f2b2aa7e69
Rust Wizard <rustwizard.0@gmail.com>
2025-04-23 17:33:25 +0300

    fmt

--- src/instance/mod.rs
+++ src/instance/mod.rs
@@ -15,28 +15,32 @@ pub struct PostgresDB {
     pub labels: HashMap<String, String>,
 }
 
-pub async fn new(dsn: String, excluded_dbnames:  Vec<String>, labels: HashMap<String, String>) -> PostgresDB {
-    let pool = match PgPoolOptions::new()
-            .max_connections(10)
-            .connect(&dsn)
-            .await
-        {
-            Ok(pool) => {
-                println!("✅Connection to the database is successful!");
-                pool
-            }
-            Err(err) => {
-                println!("🔥 Failed to connect to the database: {:?}", err);
-                std::process::exit(1);
-            }
-        };
+pub async fn new(
+    dsn: String,
+    excluded_dbnames: Vec<String>,
+    labels: HashMap<String, String>,
+) -> PostgresDB {
+    let pool = match PgPoolOptions::new().max_connections(10).connect(&dsn).await {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
 
-        PostgresDB::new(pool, excluded_dbnames, labels)
+    PostgresDB::new(pool, excluded_dbnames, labels)
 }
 
 impl PostgresDB {
-    pub fn new(db: Pool<Postgres>, excluded_dbnames:  Vec<String>, labels: HashMap<String, String>) -> PostgresDB {
-        PostgresDB{
+    pub fn new(
+        db: Pool<Postgres>,
+        excluded_dbnames: Vec<String>,
+        labels: HashMap<String, String>,
+    ) -> Self {
+        Self {
             db: db,
             exclude_db_names: excluded_dbnames,
             labels: labels,
