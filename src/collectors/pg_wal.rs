440e58748c9f81d74ea64e8d4d766eb806412ef0
Rust Wizard <rustwizard.0@gmail.com>
2025-06-22 00:09:35 +0300

    pg wal stats struct

--- src/collectors/pg_wal.rs
+++ src/collectors/pg_wal.rs
@@ -1,3 +1,5 @@
+use bigdecimal::BigDecimal;
+
 const POSTGRES_WAL_QUERY96: &str =
     "SELECT pg_is_in_recovery()::int AS recovery, 
 		(CASE pg_is_in_recovery() WHEN 't' THEN pg_last_xlog_receive_location() ELSE pg_current_xlog_location() END) - '0/00000000' AS wal_written";
@@ -11,3 +13,16 @@ const POSTGRES_WAL_QUERY_LATEST: &str =
 		(CASE pg_is_in_recovery() WHEN 't' THEN pg_last_wal_receive_lsn() - '0/00000000' ELSE pg_current_wal_lsn() - '0/00000000' END) AS wal_written, 
 		wal_bytes, wal_buffers_full, wal_write, wal_sync, wal_write_time, wal_sync_time, extract('epoch' from stats_reset) as reset_time 
 		FROM pg_stat_wal";
+
+pub struct PGWALStats {
+    recovery: i64,
+    wal_records: i64,
+    wal_fpi: i64,
+    wal_written: BigDecimal,
+    wal_bytes: BigDecimal,
+    wal_buffers_full: i64,
+    wal_write: i64,
+    wal_sync: i64,
+    wal_write_time: f64,
+    reset_time: BigDecimal
+}
