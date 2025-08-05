9934b3b02236fd31769d4ced1ecd489653b2e13f
Rust Wizard <rustwizard.0@gmail.com>
2025-08-05 17:40:51 +0300

    todo

--- src/collectors/pg_stat_io.rs
+++ src/collectors/pg_stat_io.rs
@@ -32,9 +32,9 @@ const POSTGRES_STAT_IO_LATEST: &str = "SELECT backend_type, object, context, COA
 pub struct PGStatIOStats {
     backend_type: String, // a backend type like "autovacuum worker"
     #[sqlx(rename = "object")]
-    io_object: String,    // "relation" or "temp relation"
+    io_object: String, // "relation" or "temp relation"
     #[sqlx(rename = "context")]
-    io_context: String,   // "normal", "vacuum", "bulkread" or "bulkwrite"
+    io_context: String, // "normal", "vacuum", "bulkread" or "bulkwrite"
     reads: i64,
     read_time: f64,
     writes: i64,
@@ -95,7 +95,6 @@ pub fn new(dbi: Arc<instance::PostgresDB>) -> Option<PGStatIOCollector> {
     } else {
         None
     }
-    
 }
 
 impl PGStatIOCollector {
@@ -147,7 +146,6 @@ impl Collector for PGStatIOCollector {
             .with_label_values(vals.as_slice())
             .set(data_lock.reads);
 
-
         mfs.extend(self.reads.collect());
 
         mfs
@@ -157,27 +155,27 @@ impl Collector for PGStatIOCollector {
 #[async_trait]
 impl PG for PGStatIOCollector {
     async fn update(&self) -> Result<(), anyhow::Error> {
-        let maybe_pg_statio_stats = if self.dbi.cfg.pg_version < POSTGRES_V18 {
+        let pg_statio_stats_rows = if self.dbi.cfg.pg_version < POSTGRES_V18 {
             sqlx::query_as::<_, PGStatIOStats>(POSTGRES_STAT_IO_QUERY17)
-                .fetch_optional(&self.dbi.db)
+                .fetch_all(&self.dbi.db)
                 .await?
         } else {
             sqlx::query_as::<_, PGStatIOStats>(POSTGRES_STAT_IO_LATEST)
-                .fetch_optional(&self.dbi.db)
+                .fetch_all(&self.dbi.db)
                 .await?
         };
 
-        if let Some(pg_statio_stats) = maybe_pg_statio_stats {
-            let mut data_lock = match self.data.write() {
-                Ok(data_lock) => data_lock,
-                Err(e) => bail!("can't unwrap lock. {}", e)
-            };
+        let mut data_lock = match self.data.write() {
+            Ok(data_lock) => data_lock,
+            Err(e) => bail!("can't unwrap lock. {}", e),
+        };
 
-            data_lock.reads = pg_statio_stats.reads;
-            data_lock.backend_type = pg_statio_stats.backend_type;
-            data_lock.io_context = pg_statio_stats.io_context;
-            data_lock.io_object = pg_statio_stats.io_object;
-            
+        // TODO: rethink how we colect and update our metrics. There is something wrong!!!
+        for statio in pg_statio_stats_rows {
+            data_lock.backend_type = statio.backend_type;
+            data_lock.io_context = statio.io_context;
+            data_lock.io_object = statio.io_object;
+            data_lock.reads = statio.reads;
         }
 
         Ok(())
