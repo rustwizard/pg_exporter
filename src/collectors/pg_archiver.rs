c69ee0209cc94a411453c9889cf477d53f88a468
Rust Wizard <rustwizard.0@gmail.com>
2025-08-13 12:44:42 +0300

    pg archiver stats struct

--- src/collectors/pg_archiver.rs
+++ src/collectors/pg_archiver.rs
@@ -11,3 +11,11 @@ const POSTGRES_WAL_ARCHIVING_QUERY: &str = "SELECT archived_count, failed_count,
 	EXTRACT(EPOCH FROM now() - last_archived_time) AS since_last_archive_seconds, 
 	(SELECT count(*) FROM pg_ls_archive_statusdir() WHERE name ~'.ready') AS lag_files 
 	FROM pg_stat_archiver WHERE archived_count > 0";
+
+#[derive(sqlx::FromRow, Debug)]
+pub struct PGArchiverStats {
+    archived: f64,
+    failed: f64,
+    since_archived_secinds: f64,
+    lag_files: f64,
+}
