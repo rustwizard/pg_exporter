d48150497d6c61652e5ae74581010aed5bb69e8c
Rust Wizard <rustwizard.0@gmail.com>
2025-08-18 16:45:26 +0300

    pg conflicts collector

---
+++ src/collectors/pg_conflict.rs
@@ -0,0 +1,19 @@
+use std::sync::{Arc, RwLock};
+
+use anyhow::bail;
+use async_trait::async_trait;
+
+use prometheus::core::{Collector, Desc, Opts};
+use prometheus::proto::MetricFamily;
+use prometheus::{Gauge, IntCounter, IntGauge};
+
+use crate::collectors::{PG, POSTGRES_V15};
+use crate::instance;
+
+const POSTGRES_DATABASE_CONFLICT15: &str = "SELECT datname AS database, 
+    confl_tablespace, confl_lock, confl_snapshot, confl_bufferpin, confl_deadlock
+    FROM pg_stat_database_conflicts WHERE pg_is_in_recovery() = 't'";
+
+const POSTGRES_DATABASE_CONFLICT_LATEST: &str = "SELECT datname AS database, 
+confl_tablespace, confl_lock, confl_snapshot, confl_bufferpin, confl_deadlock, confl_active_logicalslot
+		FROM pg_stat_database_conflicts WHERE pg_is_in_recovery() = 't'";
