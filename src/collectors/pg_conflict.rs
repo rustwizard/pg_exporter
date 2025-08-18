e0b929ffb5d41a3f233d398f47670790b7a38872
Rust Wizard <rustwizard.0@gmail.com>
2025-08-18 16:53:35 +0300

    pg conflict stats struct

--- src/collectors/pg_conflict.rs
+++ src/collectors/pg_conflict.rs
@@ -17,3 +17,20 @@ const POSTGRES_DATABASE_CONFLICT15: &str = "SELECT datname AS database,
 const POSTGRES_DATABASE_CONFLICT_LATEST: &str = "SELECT datname AS database, 
 confl_tablespace, confl_lock, confl_snapshot, confl_bufferpin, confl_deadlock, confl_active_logicalslot
 		FROM pg_stat_database_conflicts WHERE pg_is_in_recovery() = 't'";
+
+#[derive(sqlx::FromRow, Debug)]
+pub struct PGConflictStats {
+    database: String,
+    #[sqlx(rename = "confl_tablespace")]
+    tablespace: i64,
+    #[sqlx(rename = "confl_lock")]
+    lock: i64,
+    #[sqlx(rename = "confl_snapshot")]
+    snapshot: i64,
+    #[sqlx(rename = "confl_bufferpin")]
+    bufferpin: i64,
+    #[sqlx(rename = "confl_deadlock")]
+    deadlock: i64,
+    #[sqlx(default)]
+    active_logical_slot: i64,
+}
