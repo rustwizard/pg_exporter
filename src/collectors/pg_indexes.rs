dee4808eb4c8153531e9417b6725e1d3c57e4ba9
Rust Wizard <rustwizard.0@gmail.com>
2025-09-28 12:53:10 +0300

    query idx stats

--- src/collectors/pg_indexes.rs
+++ src/collectors/pg_indexes.rs
@@ -10,30 +10,30 @@ use prometheus::{GaugeVec, IntCounterVec};
 use crate::collectors::{PG, POSTGRES_V16};
 use crate::instance;
 
-const USER_INDEXES_QUERY: &str = "SELECT current_database() AS database, schemaname AS schema, relname AS table, 
+const USER_INDEXES_QUERY: &str = "SELECT current_database() AS database, schemaname AS schema, relname AS table,
         indexrelname AS index, (i.indisprimary OR i.indisunique) AS key,
-		i.indisvalid AS isvalid, idx_scan, idx_tup_read, idx_tup_fetch, idx_blks_read, idx_blks_hit, pg_relation_size(s1.indexrelid) AS size_bytes 
-		FROM pg_stat_user_indexes s1 
-		JOIN pg_statio_user_indexes s2 USING (schemaname, relname, indexrelname) 
-		JOIN pg_index i ON (s1.indexrelid = i.indexrelid) 
+		i.indisvalid AS isvalid, idx_scan, idx_tup_read, idx_tup_fetch, idx_blks_read, idx_blks_hit, pg_relation_size(s1.indexrelid) AS size_bytes
+		FROM pg_stat_user_indexes s1
+		JOIN pg_statio_user_indexes s2 USING (schemaname, relname, indexrelname)
+		JOIN pg_index i ON (s1.indexrelid = i.indexrelid)
 		WHERE NOT EXISTS (SELECT 1 FROM pg_locks WHERE relation = s1.indexrelid AND mode = 'AccessExclusiveLock' AND granted)";
 
-const USER_INDEXES_QUERY_TOPK: &str = "WITH stat AS (SELECT schemaname AS schema, relname AS table, indexrelname AS index, 
-        (i.indisprimary OR i.indisunique) AS key, 
-		i.indisvalid AS isvalid, idx_scan, idx_tup_read, idx_tup_fetch, idx_blks_read, idx_blks_hit, pg_relation_size(s1.indexrelid) AS size_bytes, 
-		NOT i.indisvalid OR /* unused and size > 50mb */ (idx_scan = 0 AND pg_relation_size(s1.indexrelid) > 50*1024*1024) OR 
-		(row_number() OVER (ORDER BY idx_scan DESC NULLS LAST) < $1) OR (row_number() OVER (ORDER BY idx_tup_read DESC NULLS LAST) < $1) OR 
-		(row_number() OVER (ORDER BY idx_tup_fetch DESC NULLS LAST) < $1) OR (row_number() OVER (ORDER BY idx_blks_read DESC NULLS LAST) < $1) OR 
-		(row_number() OVER (ORDER BY idx_blks_hit DESC NULLS LAST) < $1) OR (row_number() OVER (ORDER BY pg_relation_size(s1.indexrelid) DESC NULLS LAST) < $1) AS visible 
-		FROM pg_stat_user_indexes s1 
-		JOIN pg_statio_user_indexes s2 USING (schemaname, relname, indexrelname) 
-		JOIN pg_index i ON (s1.indexrelid = i.indexrelid) 
-		WHERE NOT EXISTS ( SELECT 1 FROM pg_locks WHERE relation = s1.indexrelid AND mode = 'AccessExclusiveLock' AND granted)) 
-		SELECT current_database() AS database, \"schema\", \"table\", \"index\", \"key\", isvalid, idx_scan, idx_tup_read, idx_tup_fetch, 
-		idx_blks_read, idx_blks_hit, size_bytes FROM stat WHERE visible 
-		UNION ALL SELECT current_database() AS database, 'all_shemas', 'all_other_tables', 'all_other_indexes', true, null, 
-		NULLIF(SUM(COALESCE(idx_scan,0)),0), NULLIF(SUM(COALESCE(idx_tup_fetch,0)),0), NULLIF(SUM(COALESCE(idx_tup_read,0)),0), 
-		NULLIF(SUM(COALESCE(idx_blks_read,0)),0), NULLIF(SUM(COALESCE(idx_blks_hit,0)),0), 
+const USER_INDEXES_QUERY_TOPK: &str = "WITH stat AS (SELECT schemaname AS schema, relname AS table, indexrelname AS index,
+        (i.indisprimary OR i.indisunique) AS key,
+		i.indisvalid AS isvalid, idx_scan, idx_tup_read, idx_tup_fetch, idx_blks_read, idx_blks_hit, pg_relation_size(s1.indexrelid) AS size_bytes,
+		NOT i.indisvalid OR /* unused and size > 50mb */ (idx_scan = 0 AND pg_relation_size(s1.indexrelid) > 50*1024*1024) OR
+		(row_number() OVER (ORDER BY idx_scan DESC NULLS LAST) < $1) OR (row_number() OVER (ORDER BY idx_tup_read DESC NULLS LAST) < $1) OR
+		(row_number() OVER (ORDER BY idx_tup_fetch DESC NULLS LAST) < $1) OR (row_number() OVER (ORDER BY idx_blks_read DESC NULLS LAST) < $1) OR
+		(row_number() OVER (ORDER BY idx_blks_hit DESC NULLS LAST) < $1) OR (row_number() OVER (ORDER BY pg_relation_size(s1.indexrelid) DESC NULLS LAST) < $1) AS visible
+		FROM pg_stat_user_indexes s1
+		JOIN pg_statio_user_indexes s2 USING (schemaname, relname, indexrelname)
+		JOIN pg_index i ON (s1.indexrelid = i.indexrelid)
+		WHERE NOT EXISTS ( SELECT 1 FROM pg_locks WHERE relation = s1.indexrelid AND mode = 'AccessExclusiveLock' AND granted))
+		SELECT current_database() AS database, \"schema\", \"table\", \"index\", \"key\", isvalid, idx_scan, idx_tup_read, idx_tup_fetch,
+		idx_blks_read, idx_blks_hit, size_bytes FROM stat WHERE visible
+		UNION ALL SELECT current_database() AS database, 'all_shemas', 'all_other_tables', 'all_other_indexes', true, null,
+		NULLIF(SUM(COALESCE(idx_scan,0)),0), NULLIF(SUM(COALESCE(idx_tup_fetch,0)),0), NULLIF(SUM(COALESCE(idx_tup_read,0)),0),
+		NULLIF(SUM(COALESCE(idx_blks_read,0)),0), NULLIF(SUM(COALESCE(idx_blks_hit,0)),0),
 		NULLIF(SUM(COALESCE(size_bytes,0)),0) FROM stat WHERE NOT visible HAVING EXISTS (SELECT 1 FROM stat WHERE NOT visible)";
 
 #[derive(sqlx::FromRow, Debug)]
@@ -242,6 +242,16 @@ impl Collector for PGIndexesCollector {
 #[async_trait]
 impl PG for PGIndexesCollector {
     async fn update(&self) -> Result<(), anyhow::Error> {
+        let mut pg_idx_stats_rows = if self.dbi.cfg.pg_collect_topidx > 0 {
+            sqlx::query_as::<_, PGIndexesStats>(USER_INDEXES_QUERY_TOPK)
+                .fetch_all(&self.dbi.db)
+                .await?
+        } else {
+            sqlx::query_as::<_, PGIndexesStats>(USER_INDEXES_QUERY)
+                .fetch_all(&self.dbi.db)
+                .await?
+        };
+
         Ok(())
     }
 }
