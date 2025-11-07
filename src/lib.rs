882a078044234ac02195621d738ec630b1300a58
Rust Wizard <rustwizard.0@gmail.com>
2025-11-07 15:26:04 +0300

    add lib.rs file

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+pub fn logger_init() {
+    // TODO: get debug flag from config or env and set log level.
+
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than INFO (e.g, info, error, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::INFO)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+}
