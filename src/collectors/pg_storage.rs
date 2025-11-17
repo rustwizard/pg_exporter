13344d8bf63325ed91008904e961ac37bc8a44c2
Rust Wizard <rustwizard.0@gmail.com>
2025-11-17 14:49:05 +0300

    stubs for pg storage collector implementations

--- src/collectors/pg_storage.rs
+++ src/collectors/pg_storage.rs
@@ -1,4 +1,17 @@
 use rust_decimal::Decimal;
+use rust_decimal::prelude::ToPrimitive;
+
+use std::sync::{Arc, RwLock};
+
+use anyhow::bail;
+use async_trait::async_trait;
+
+use prometheus::core::{Collector, Desc, Opts};
+use prometheus::{GaugeVec, IntGaugeVec, proto};
+use tracing::error;
+
+use crate::collectors::PG;
+use crate::instance;
 
 const POSTGRES_TEMP_FILES_INFLIGHT: &str = "SELECT ts.spcname AS tablespace, COALESCE(COUNT(size), 0) AS files_total, COALESCE(sum(size), 0) AS bytes_total, 
 		COALESCE(EXTRACT(EPOCH FROM clock_timestamp() - min(modification)), 0) AS max_age_seconds 
@@ -11,4 +24,46 @@ pub struct PGStorageStats {
     files_total: Option<i64>,
     bytes_total: Option<Decimal>,
     max_age_seconds: Option<Decimal>,
+}
+
+// PGStorageCollector exposing various stats related to Postgres storage layer.
+// This stats observed using different stats sources.
+#[derive(Debug, Clone)]
+pub struct PGStorageCollector {
+    dbi: Arc<instance::PostgresDB>,
+    data: Arc<RwLock<Vec<PGStorageStats>>>,
+    descs: Vec<Desc>,
+}
+
+pub fn new(dbi: Arc<instance::PostgresDB>) -> Option<PGStorageCollector> {
+    match PGStorageCollector::new(dbi) {
+        Ok(result) => Some(result),
+        Err(e) => {
+            error!("error when create pg storage collector: {}", e);
+            None
+        }
+    }
+}
+
+impl PGStorageCollector {
+    fn new(dbi: Arc<instance::PostgresDB>) -> anyhow::Result<Self> {
+        todo!()
+    }
+}
+
+impl Collector for PGStorageCollector {
+    fn desc(&self) -> Vec<&Desc> {
+        self.descs.iter().collect()
+    }
+
+    fn collect(&self) -> Vec<proto::MetricFamily> {
+        todo!()
+    }
+}
+
+#[async_trait]
+impl PG for PGStorageCollector {
+    async fn update(&self) -> Result<(), anyhow::Error> {
+        Ok(())
+    }
 }
