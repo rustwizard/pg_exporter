7afd90ed6cc14f1042ef58c8b3636bdcd4d09b51
Rust Wizard <rustwizard.0@gmail.com>
2025-11-17 14:39:58 +0300

    add pg storage stat struct

--- src/collectors/pg_storage.rs
+++ src/collectors/pg_storage.rs
@@ -1,4 +1,14 @@
+use rust_decimal::Decimal;
+
 const POSTGRES_TEMP_FILES_INFLIGHT: &str = "SELECT ts.spcname AS tablespace, COALESCE(COUNT(size), 0) AS files_total, COALESCE(sum(size), 0) AS bytes_total, 
 		COALESCE(EXTRACT(EPOCH FROM clock_timestamp() - min(modification)), 0) AS max_age_seconds 
 		FROM pg_tablespace ts LEFT JOIN (SELECT spcname,(pg_ls_tmpdir(oid)).* FROM pg_tablespace WHERE spcname != 'pg_global') ls ON ls.spcname = ts.spcname 
 		WHERE ts.spcname != 'pg_global' GROUP BY ts.spcname";
+
+#[derive(sqlx::FromRow, Debug, Default)]
+pub struct PGStorageStats {
+    tablespace: Option<String>,
+    files_total: Option<i64>,
+    bytes_total: Option<Decimal>,
+    max_age_seconds: Option<Decimal>,
+}
