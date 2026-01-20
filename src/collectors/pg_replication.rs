05ce3fe8c284721161613daa103acfc75d054f45
Rust Wizard <rustwizard.0@gmail.com>
2026-01-20 12:13:05 +0300

    postgres replication collector

---
+++ src/collectors/pg_replication.rs
@@ -0,0 +1,8 @@
+use std::sync::{Arc, RwLock};
+
+use anyhow::bail;
+use async_trait::async_trait;
+
+use prometheus::core::{Collector, Desc, Opts};
+use prometheus::{IntGaugeVec, proto};
+use tracing::{error, info};
