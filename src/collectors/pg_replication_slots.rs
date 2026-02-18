cdb955f850c380fdfc64a8c887ef7ac1f7f5f843
Rust Wizard <rustwizard.0@gmail.com>
2026-02-18 15:17:54 +0300

    replication slots collector

---
+++ src/collectors/pg_replication_slots.rs
@@ -0,0 +1,83 @@
+use rust_decimal::Decimal;
+use rust_decimal::prelude::ToPrimitive;
+use std::sync::{Arc, RwLock};
+
+use anyhow::bail;
+use async_trait::async_trait;
+
+use crate::instance;
+use prometheus::core::{Collector, Desc, Opts};
+use prometheus::{IntGaugeVec, proto};
+use tracing::{error, info};
+
+use crate::collectors::{PG, POSTGRES_V10, POSTGRES_V96};
+
+// Query for Postgres version 9.6 and older.
+const POSTGRES_REPLICATION_QUERY96: &str = "SELECT database, slot_name, slot_type, active, 
+		CASE WHEN pg_is_in_recovery() THEN pg_xlog_location_diff(pg_last_xlog_receive_location(), restart_lsn) 
+		ELSE pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn) END AS since_restart_bytes 
+		FROM pg_replication_slots";
+
+// Query for Postgres versions from 10 and newer.
+const POSTGRES_REPLICATION_QUERY_LATEST: &str = "SELECT database, slot_name, slot_type, active, 
+    CASE WHEN pg_is_in_recovery() THEN pg_wal_lsn_diff(pg_last_wal_receive_lsn(), restart_lsn) 
+    ELSE pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn) END AS since_restart_bytes 
+    FROM pg_replication_slots";
+
+#[derive(sqlx::FromRow, Debug, Default)]
+pub struct PGReplicationSlotsStats {
+    database: Option<String>,
+    slot_name: Option<String>,
+    slot_type: Option<String>,
+    active: Option<bool>,
+    since_restart_bytes: Option<Decimal>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PGReplicationSlotsCollector {
+    dbi: Arc<instance::PostgresDB>,
+    data: Arc<RwLock<Vec<PGReplicationSlotsStats>>>,
+    descs: Vec<Desc>,
+}
+
+pub fn new(dbi: Arc<instance::PostgresDB>) -> Option<PGReplicationSlotsCollector> {
+    // Collecting pg_replication since Postgres 9.6.
+    if dbi.cfg.pg_version >= POSTGRES_V96 {
+        match PGReplicationSlotsCollector::new(dbi) {
+            Ok(result) => Some(result),
+            Err(e) => {
+                error!("error when create pg replication slots collector: {}", e);
+                None
+            }
+        }
+    } else {
+        info!("some server-side functions are not available, required Postgres 9.6 or newer");
+        None
+    }
+}
+
+impl PGReplicationSlotsCollector {
+    fn new(dbi: Arc<instance::PostgresDB>) -> anyhow::Result<Self> {
+        todo!()
+    }
+}
+
+impl Collector for PGReplicationSlotsCollector {
+    fn desc(&self) -> Vec<&Desc> {
+        self.descs.iter().collect()
+    }
+
+    fn collect(&self) -> Vec<proto::MetricFamily> {
+        // collect MetricFamilies.
+        let mut mfs = Vec::with_capacity(4);
+
+        mfs
+    }
+}
+
+#[async_trait]
+impl PG for PGReplicationSlotsCollector {
+    async fn update(&self) -> Result<(), anyhow::Error> {
+        Ok(())
+    }
+}
